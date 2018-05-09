d8841dcb4cfc388290949aa621666c6e0fed7863
James Mallon <jamesmallondev@gmail.com>
2018-05-09 15:21:18 -0700

    Removing vendor folder

--- dao/StoryDAO.go
+++ dao/StoryDAO.go
@@ -75,28 +75,73 @@ func (sd *storyDAO) GetTopStoriesIds(qtt int) ([]int, error) {
 
 // Get a topstory from the specified id
 func (sd *storyDAO) GetTopStory(id int) (model.Story, error) {
+	var wg sync.WaitGroup
 	vars := sd.envVar.GetVars()
 	url := utils.StringConcat(vars["baseUrl"], fmt.Sprintf(vars["item"], id))
+
 	var story model.Story
 
-	r, err := httpClient.Get(url)
-	if err != nil {
-		return story, err
-	}
+	storiesChn := make(chan storiesChnRtn)
 
-	defer r.Body.Close()
-	response, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return story, err
-	}
+	go func() {
+		r, err := httpClient.Get(url)
+		if err != nil {
+			storiesChn <- storiesChnRtn{
+				Result: story,
+				Err:    err,
+			}
+			runtime.Goexit()
+		}
 
-	err = json.Unmarshal(response, &story)
-	if err != nil {
-		return story, err
-	}
+		defer r.Body.Close()
+		response, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			storiesChn <- storiesChnRtn{
+				Result: story,
+				Err:    err,
+			}
+			runtime.Goexit()
+		}
 
-	return story, err
+		err = json.Unmarshal(response, &story)
+		if err != nil {
+			storiesChn <- storiesChnRtn{
+				Result: story,
+				Err:    err,
+			}
+		}
+		defer wg.Done()
+	}()
+	resp := <-storiesChn
+	defer close(storiesChn)
+	wg.Wait()
+	return resp.Result.(model.Story), resp.Err
 }
+
+// Get a topstory from the specified id
+//func (sd *storyDAO) GetTopStory(id int) (model.Story, error) {
+//	vars := sd.envVar.GetVars()
+//	url := utils.StringConcat(vars["baseUrl"], fmt.Sprintf(vars["item"], id))
+//	var story model.Story
+//
+//	r, err := httpClient.Get(url)
+//	if err != nil {
+//		return story, err
+//	}
+//
+//	defer r.Body.Close()
+//	response, err := ioutil.ReadAll(r.Body)
+//	if err != nil {
+//		return story, err
+//	}
+//
+//	err = json.Unmarshal(response, &story)
+//	if err != nil {
+//		return story, err
+//	}
+//
+//	return story, err
+//}
 
 // Get a newstory from the specified id
 func (sd *storyDAO) GetNewStory(id int) {}
